fe73eb0f2c49293ccb9f170e185e2c8e5eaef34b
liqiuzhuang <iautumns@163.com>
2023-06-10 11:26:14 +0800

    Update autumn_cloud.sql

--- SQL/autumn_cloud.sql
+++ SQL/autumn_cloud.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `sys_dept`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_dept` (
-  `dept_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '部门id',
+  `id` varchar(50) NOT NULL COMMENT '部门id',
   `parent_id` bigint(20) DEFAULT '0' COMMENT '父部门id',
   `ancestors` varchar(50) DEFAULT '' COMMENT '祖级列表',
   `dept_name` varchar(30) DEFAULT '' COMMENT '部门名称',
@@ -37,8 +37,8 @@ CREATE TABLE `sys_dept` (
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`dept_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8mb4 COMMENT='部门表';
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='部门表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +47,7 @@ CREATE TABLE `sys_dept` (
 
 LOCK TABLES `sys_dept` WRITE;
 /*!40000 ALTER TABLE `sys_dept` DISABLE KEYS */;
-INSERT INTO `sys_dept` VALUES (100,0,'0','若依科技',0,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:49','',NULL),(101,100,'0,100','深圳总公司',1,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:49','',NULL),(102,100,'0,100','长沙分公司',2,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:49','',NULL),(103,101,'0,100,101','研发部门',1,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:49','',NULL),(104,101,'0,100,101','市场部门',2,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:49','',NULL),(105,101,'0,100,101','测试部门',3,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:50','',NULL),(106,101,'0,100,101','财务部门',4,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:50','',NULL),(107,101,'0,100,101','运维部门',5,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:50','',NULL),(108,102,'0,100,102','市场部门',1,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:50','',NULL),(109,102,'0,100,102','财务部门',2,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:50','',NULL);
+INSERT INTO `sys_dept` VALUES ('100',0,'0','若依科技',0,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:49','',NULL),('101',100,'0,100','深圳总公司',1,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:49','',NULL),('102',100,'0,100','长沙分公司',2,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:49','',NULL),('103',101,'0,100,101','研发部门',1,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:49','',NULL),('104',101,'0,100,101','市场部门',2,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:49','',NULL),('105',101,'0,100,101','测试部门',3,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:50','',NULL),('106',101,'0,100,101','财务部门',4,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:50','',NULL),('107',101,'0,100,101','运维部门',5,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:50','',NULL),('108',102,'0,100,102','市场部门',1,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:50','',NULL),('109',102,'0,100,102','财务部门',2,'若依','15888888888','ry@qq.com','0','0','admin','2023-05-25 03:10:50','',NULL);
 /*!40000 ALTER TABLE `sys_dept` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,10 +59,10 @@ DROP TABLE IF EXISTS `sys_menu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_menu` (
-  `menu_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '菜单ID',
+  `id` varchar(50) NOT NULL COMMENT '菜单ID',
   `parent_id` bigint(20) DEFAULT '0' COMMENT '父菜单ID',
   `path` varchar(200) DEFAULT '' COMMENT '路由地址',
-  `name` varchar(50) DEFAULT '' COMMENT 'name',
+  `name` varchar(50) DEFAULT '' COMMENT '菜单 name',
   `redirect` varchar(255) DEFAULT NULL COMMENT '重定向地址',
   `component` varchar(255) DEFAULT NULL COMMENT '组件路径',
   `icon` varchar(100) DEFAULT '#' COMMENT '菜单图标',
@@ -84,8 +84,8 @@ CREATE TABLE `sys_menu` (
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`menu_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COMMENT='菜单权限表';
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='菜单权限表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,7 +94,7 @@ CREATE TABLE `sys_menu` (
 
 LOCK TABLES `sys_menu` WRITE;
 /*!40000 ALTER TABLE `sys_menu` DISABLE KEYS */;
-INSERT INTO `sys_menu` VALUES (1,0,'/home/index','home',NULL,'/home/index','HomeFilled','首页','','1','1','1','1','0',0,'','1',NULL,NULL,'','1','admin','2023-06-09 20:52:29','admin','2023-06-09 21:59:55'),(2,0,'/system','system',NULL,NULL,'Menu','系统管理','','1','1','1','1','0',0,'','1',NULL,NULL,'','1','admin','2023-06-09 20:52:29','admin','2023-06-09 21:33:01'),(3,2,'/system/menuMange','menuMange',NULL,'/system/menuMange/menuList','Apple','菜单管理','','1','1','1','1','0',0,'','1',NULL,NULL,'','1','admin','2023-06-09 20:52:29','admin','2023-06-09 21:29:05'),(11,0,'/test','test',NULL,'/test/testList','Baseball','测试','','1','1','1','1','1',0,'','1',NULL,NULL,'','0','admin','2023-06-09 21:37:07','admin','2023-06-09 21:55:03'),(12,0,'1','1','1','1','Aim','1','','1','1','1','1','1',0,'','1',NULL,NULL,'','0','admin','2023-06-09 21:38:46','admin','2023-06-09 21:41:56'),(13,2,'2','2','','2','ArrowUp','2','','1','1','1','1','1',0,'','1',NULL,NULL,'','0','admin','2023-06-09 21:55:19','',NULL),(14,2,'3','3','3','3','AlarmClock','3','','1','1','1','1','1',0,'','1',NULL,NULL,'','0','admin','2023-06-09 21:57:47','',NULL);
+INSERT INTO `sys_menu` VALUES ('1',0,'/home/index','home',NULL,'/home/index','HomeFilled','首页','','1','1','1','1','0',1,'1','1',NULL,NULL,'','1','admin','2023-06-09 20:52:29','admin','2023-06-10 10:41:32'),('2',0,'/system',NULL,NULL,NULL,'Tools','系统管理','','1','1','1','1','0',2,'0','1',NULL,NULL,'','1','admin','2023-06-09 20:52:29','admin','2023-06-10 10:43:31'),('3',2,'/system/menuMange','menuMange',NULL,'/system/menuMange/menuList','Menu','菜单管理','','1','1','1','1','0',1,'1','1',NULL,NULL,'','1','admin','2023-06-09 20:52:29','admin','2023-06-10 10:43:59');
 /*!40000 ALTER TABLE `sys_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,7 +106,7 @@ DROP TABLE IF EXISTS `sys_post`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_post` (
-  `post_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '岗位ID',
+  `id` varchar(50) NOT NULL COMMENT '岗位ID',
   `post_code` varchar(64) NOT NULL COMMENT '岗位编码',
   `post_name` varchar(50) NOT NULL COMMENT '岗位名称',
   `post_sort` int(11) NOT NULL COMMENT '显示顺序',
@@ -117,8 +117,8 @@ CREATE TABLE `sys_post` (
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='岗位信息表';
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='岗位信息表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,7 +127,7 @@ CREATE TABLE `sys_post` (
 
 LOCK TABLES `sys_post` WRITE;
 /*!40000 ALTER TABLE `sys_post` DISABLE KEYS */;
-INSERT INTO `sys_post` VALUES (1,'ceo','董事长',1,'','0','0','admin','2023-05-25 03:10:50','',NULL),(2,'se','项目经理',2,'','0','0','admin','2023-05-25 03:10:50','',NULL),(3,'hr','人力资源',3,'','0','0','admin','2023-05-25 03:10:50','',NULL),(4,'user','普通员工',4,'','0','0','admin','2023-05-25 03:10:50','',NULL);
+INSERT INTO `sys_post` VALUES ('1','ceo','董事长',1,'','0','0','admin','2023-05-25 03:10:50','',NULL),('2','se','项目经理',2,'','0','0','admin','2023-05-25 03:10:50','',NULL),('3','hr','人力资源',3,'','0','0','admin','2023-05-25 03:10:50','',NULL),('4','user','普通员工',4,'','0','0','admin','2023-05-25 03:10:50','',NULL);
 /*!40000 ALTER TABLE `sys_post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,13 +139,13 @@ DROP TABLE IF EXISTS `sys_role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_role` (
-  `role_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `id` varchar(50) NOT NULL COMMENT '角色ID',
   `role_name` varchar(30) NOT NULL COMMENT '角色名称',
   `role_key` varchar(100) NOT NULL COMMENT '角色权限字符串',
   `role_sort` int(11) NOT NULL COMMENT '显示顺序',
   `data_scope` char(1) DEFAULT '1' COMMENT '数据范围（1：全部数据权限 2：自定数据权限 3：本部门数据权限 4：本部门及以下数据权限）',
-  `menu_check_strictly` tinyint(1) DEFAULT '1' COMMENT '菜单树选择项是否关联显示',
-  `dept_check_strictly` tinyint(1) DEFAULT '1' COMMENT '部门树选择项是否关联显示',
+  `menu_check_strictly` char(1) DEFAULT '1' COMMENT '菜单树选择项是否关联显示（0是 1否）',
+  `dept_check_strictly` char(1) DEFAULT '1' COMMENT '部门树选择项是否关联显示（0是 1否）',
   `remark` varchar(500) DEFAULT NULL COMMENT '备注',
   `status` char(1) NOT NULL DEFAULT '1' COMMENT '是否停用（0是 1否）',
   `del_flag` char(1) DEFAULT '1' COMMENT '删除标志（0代表删除 1代表存在）',
@@ -153,7 +153,7 @@ CREATE TABLE `sys_role` (
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`role_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色信息表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -174,15 +174,15 @@ DROP TABLE IF EXISTS `sys_role_dept`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_role_dept` (
-  `role_dept_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `role_id` bigint(20) NOT NULL COMMENT '角色ID',
-  `dept_id` bigint(20) NOT NULL COMMENT '部门ID',
+  `id` varchar(50) NOT NULL COMMENT '角色部门关联表ID',
+  `role_id` varchar(50) NOT NULL COMMENT '角色ID',
+  `dept_id` varchar(50) NOT NULL COMMENT '部门ID',
   `del_flag` char(1) DEFAULT '1' COMMENT '删除标志（0代表删除 1代表存在）',
   `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`role_dept_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色和部门关联表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -203,15 +203,15 @@ DROP TABLE IF EXISTS `sys_role_menu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_role_menu` (
-  `role_menu_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `role_id` bigint(20) NOT NULL COMMENT '角色ID',
-  `menu_id` bigint(20) NOT NULL COMMENT '菜单ID',
+  `id` varchar(50) NOT NULL COMMENT '角色菜单关联表ID',
+  `role_id` varchar(50) NOT NULL COMMENT '角色ID',
+  `menu_id` varchar(50) NOT NULL COMMENT '菜单ID',
   `del_flag` char(1) DEFAULT '1' COMMENT '删除标志（0代表删除 1代表存在）',
   `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`role_menu_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色和菜单关联表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -232,8 +232,8 @@ DROP TABLE IF EXISTS `sys_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_user` (
-  `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `dept_id` bigint(20) DEFAULT NULL COMMENT '部门ID',
+  `id` varchar(50) NOT NULL COMMENT '用户ID',
+  `dept_id` varchar(50) DEFAULT NULL COMMENT '部门ID',
   `user_name` varchar(30) NOT NULL COMMENT '用户账号',
   `nick_name` varchar(30) NOT NULL COMMENT '用户昵称',
   `user_type` varchar(2) DEFAULT '00' COMMENT '用户类型（00系统用户）',
@@ -251,8 +251,8 @@ CREATE TABLE `sys_user` (
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -261,7 +261,7 @@ CREATE TABLE `sys_user` (
 
 LOCK TABLES `sys_user` WRITE;
 /*!40000 ALTER TABLE `sys_user` DISABLE KEYS */;
-INSERT INTO `sys_user` VALUES (1,NULL,'admin','管理员','00','','','0','','6186b6850150072ea3981c523e0180b34d8285172161fdd8e1e2d263dae3d3f91fe78873d4b698c0873d7657e7571ba97bfae48a8f10c07606355a9fb7087754',NULL,'0','',NULL,'1','',NULL,'',NULL);
+INSERT INTO `sys_user` VALUES ('1',NULL,'admin','管理员','00','','','0','http://dummyimage.com/100x100','6186b6850150072ea3981c523e0180b34d8285172161fdd8e1e2d263dae3d3f91fe78873d4b698c0873d7657e7571ba97bfae48a8f10c07606355a9fb7087754',NULL,'0','',NULL,'1','',NULL,'',NULL);
 /*!40000 ALTER TABLE `sys_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -302,15 +302,15 @@ DROP TABLE IF EXISTS `sys_user_role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_user_role` (
-  `user_role_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `user_id` bigint(20) NOT NULL COMMENT '用户ID',
-  `role_id` bigint(20) NOT NULL COMMENT '角色ID',
+  `id` varchar(50) NOT NULL COMMENT '用户角色关联表ID',
+  `user_id` varchar(50) NOT NULL COMMENT '用户ID',
+  `role_id` varchar(50) NOT NULL COMMENT '角色ID',
   `del_flag` char(1) DEFAULT '1' COMMENT '删除标志（0代表删除 1代表存在）',
   `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`user_role_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户和角色关联表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -332,4 +332,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-09 22:15:36
+-- Dump completed on 2023-06-10 11:26:06
