c754eb4f99e80322bae3adb14462097630cbe28c
autumn <1060590974@qq.com>
2023-07-11 00:07:19 +0800

    按钮权限

--- autumn-system/src/main/java/com/autumn/menu/controller/BtnController.java
+++ autumn-system/src/main/java/com/autumn/menu/controller/BtnController.java
@@ -2,6 +2,7 @@ package com.autumn.menu.controller;
 
 import com.autumn.menu.service.MenuService;
 import com.autumn.result.Result;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping("/btn")
+@RequestMapping("/buttons")
 public class BtnController {
 
     @Resource
     private MenuService menuService;
 
-    @PostMapping
+    @GetMapping
     public Result getBtnsList() {
         return menuService.getBtnsList();
     }
