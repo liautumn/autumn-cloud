d6a4acc26d967c961bcd818dcb1632f9b48178e8
autumn <1060590974@qq.com>
2023-07-22 13:07:02 +0800

    docker部署优化

---
+++ Docker/deploy.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+# 使用说明，用来提示输入参数
+usage() {
+	echo "Usage: sh 执行脚本.sh [ port | base | admin | nginx | stop | rm ]"
+	exit 1
+}
+
+# 开启所需端口
+port(){
+	firewall-cmd --add-port=8080/tcp --permanent
+	service firewalld restart
+}
+
+# 启动基础环境（必须）
+base(){
+	docker-compose up -d autumn-mysql autumn-redis autumn-nacos autumn-minio
+}
+
+admin(){
+  gateway
+  auth
+  system
+  file
+}
+
+# 启动系统服务
+system(){
+  docker-compose stop autumn-system
+  docker-compose rm -f autumn-system
+  docker rmi -f autumn-system:latest
+  docker-compose build autumn-system
+	docker-compose up -d autumn-system
+}
+
+# 启动认证服务
+auth(){
+  docker-compose stop autumn-auth
+  docker-compose rm -f autumn-auth
+  docker rmi -f autumn-auth:latest
+  docker-compose build autumn-auth
+	docker-compose up -d autumn-auth
+}
+
+# 启动网关服务
+gateway(){
+  docker-compose stop autumn-gateway
+  docker-compose rm -f autumn-gateway
+  docker rmi -f autumn-gateway:latest
+  docker-compose build autumn-gateway
+	docker-compose up -d autumn-gateway
+}
+
+# 启动文件服务
+file(){
+  docker-compose stop autumn-file
+  docker-compose rm -f autumn-file
+  docker rmi -f autumn-file:latest
+  docker-compose build autumn-file
+	docker-compose up -d autumn-file
+}
+
+# 启动代码生成服务
+generate(){
+  docker-compose stop autumn-generate
+  docker-compose rm -f autumn-generate
+  docker rmi -f autumn-generate:latest
+  docker-compose build autumn-generate
+	docker-compose up -d autumn-generate
+}
+
+#启动前端
+nginx(){
+  docker-compose stop autumn-nginx
+  docker-compose rm -f autumn-nginx
+  docker rmi -f autumn-nginx:latest
+  docker-compose build autumn-nginx
+	docker-compose up -d autumn-nginx
+}
+
+# 关闭所有环境/模块
+stop(){
+	docker-compose stop
+}
+
+# 删除所有环境/模块
+rm(){
+	docker-compose rm
+}
+
+# 根据输入参数，选择执行对应方法，不输入则执行使用说明
+case "$1" in
+"port")
+	port
+;;
+"base")
+	base
+;;
+"admin")
+	admin
+;;
+"nginx")
+	nginx
+;;
+"stop")
+	stop
+;;
+"rm")
+	rm
+;;
+*)
+	usage
+;;
+esac
